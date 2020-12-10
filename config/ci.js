a9cee6252e2ae1b62edca377c786702c9916ad10
unknown <sandoval.ac.rafael@gmail.com>
2020-12-10 16:49:35 -0300

    Travis config

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+	googleClientID:
+	  '509062681427-1mgc2d70esr7e4uqvdrrotd11f99bi71.apps.googleusercontent.com',
+	googleClientSecret: '9q5kWxcC4jdNNEH6a6DW34no',
+	mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+	cookieKey: '123123123',
+	redisUrl: 'redis://127.0.0.1:6379'
+  };
+  
